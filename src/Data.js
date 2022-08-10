a400942a55e06f5bad6401385fad47997ac7917d
shivshaktisingh-cedcommerce <shivshaktisingh@cedcoss.com>
2022-08-10 18:31:51 +0530

    amazon clone

---
+++ src/Data.js
@@ -0,0 +1,12 @@
+
+
+const Data = [
+                {image : "./1.jpg" ,name : "Product101" , price : "250"},
+                {image : "./2.jpg" ,name : "Product102" ,  price  :"320"},
+                {image: "./3.jpg" ,name : "Product103" , price :"190"},
+                {image: "./4.jpg" ,name : "Product104" , price :"210"},
+                {image: "./5.jpg" ,name : "Product105" , price :"380" }
+                
+             ]
+
+export default Data
---
+++ src/hj/Data.js
@@ -0,0 +1,40 @@
+export const data ={
+    "books": [
+        {"bookid":0,
+        "image":"1.jpg",
+        "title":"The monk who sold his ferrari",
+        "Listprice":"500",
+        "selling":"300"
+        },
+        {"bookid":1,
+        "image":"2.jpg",
+        "title":"Sapiens",
+        "Listprice":"700",
+        "selling":"350"
+        },
+        {"bookid":2,
+        "image":"3.jpg",
+        "title":"The Subtle art of not giving a fuck",
+        "Listprice":"650",
+        "selling":"250"
+        },
+        {"bookid":3,
+        "image":"4.jpg",
+        "title":"Unposted Letter",
+        "Listprice":"900",
+        "selling":"450"
+        },
+        {"bookid":4,
+        "image":"5.jpg",
+        "title":"Eat that Frog",
+        "Listprice":"800",
+        "selling":"400"
+        },
+        {"bookid":5,
+        "image":"6.jpg",
+        "title":"The habit of winning",
+        "Listprice":"700",
+        "selling":"300"
+        },
+]
+}
